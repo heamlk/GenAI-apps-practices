48cdd0ca34e8b1ce0b6cad9afba068c72fcd17f5
heamlk <mehdiabderrazak60@gmail.com>
2025-01-03 12:20:47 +0330

    chores: debug for ai_legeal_document_summarizer

--- ai_legal_document_summarizer/ai_legal_document_summarizer_app.py
+++ ai_legal_document_summarizer/ai_legal_document_summarizer_app.py
@@ -1,12 +1,11 @@
 import os
 import faiss
-from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-
 from phi.knowledge.langchain import LangChainKnowledgeBase
 from phi.agent import Agent
-
-from langchain_community.document_loaders import DirectoryLoader, JSONLoader
+from langchain_community.document_loaders import JSONLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain_openai import OpenAIEmbeddings
@@ -18,12 +17,7 @@ load_dotenv()
 openai_api_key = os.getenv('OPENAI_API_KEY')
 app = FastAPI()
 
-loader = DirectoryLoader(
-    "../alaska-federal-reports",
-    glob="**/*.json",
-    loader_cls=JSONLoader
-)
-documents = loader.load()
+
 
 embedding = OpenAIEmbeddings()
 index = faiss.IndexFlatL2(len(embedding.embed_query("hello world")))
@@ -34,6 +28,21 @@ vector_store = FAISS(
     docstore=InMemoryDocstore(),
     index_to_docstore_id={}
 )
+
+directory_path = "law_corpuses/alaska-federal-reports"
+file_list = [
+    os.path.join(root, file)
+    for root, _, files in os.walk(directory_path)
+    for file in files if file.endswith(".json")
+]
+documents = []
+
+for file_path in file_list:
+    loader = JSONLoader(jq_schema='.', file_path=file_path,
+                        text_content=False)
+    file_documents = loader.load()
+    documents.extend(file_documents)
+
 
 vector_store.add_documents(documents)
 retriever = vector_store.as_retriever()
@@ -53,11 +62,15 @@ agent = Agent(
     ]
 )
 
+
+class QueryRequest(BaseModel):
+    query: str
+
 @app.post("/query")
-async def query_agent(request: Request):
-    data = await request.json()
-    user_query = data.get("query", "")
+async def query_agent(request: QueryRequest):
+    user_query = request.query
     if not user_query:
         return JSONResponse({"error": "No query provided"}, status_code=400)
     answer = agent.run(user_query)
+    answer = answer.to_dict().get('content')
     return JSONResponse({"response": answer})
